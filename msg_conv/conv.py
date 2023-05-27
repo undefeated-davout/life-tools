d327fe6cfe10437eec5991e92722f2215fd6e692
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2023-05-27 13:04:24 +0900

    update: 添付ファイルを取り出して保存できるように

--- msg_conv/conv.py
+++ msg_conv/conv.py
@@ -1,5 +1,20 @@
+import os
 import extract_msg
 
 msg = extract_msg.Message("test.msg")
+
+# Get the email body
 body = msg.body
 print(body)
+
+# Get attachments
+attachments = msg.attachments
+
+# Create a directory for the attachments if it does not exist
+if not os.path.isdir("attachments"):
+    os.makedirs("attachments")
+
+# Save all attachments in the directory
+for attachment in attachments:
+    with open(f"attachments/{attachment.longFilename}", "wb") as fp:
+        fp.write(attachment.data)
